a97d594cfe62e81ad8a3419f14844c5cc14df9d0
mercymam <mercymamedu12@gmail.com>
2025-06-17 08:47:28 +0100

    Makes the process asynchronous

--- text_extractor/extract_text.py
+++ text_extractor/extract_text.py
@@ -1,19 +1,24 @@
 import logging
+import asyncio
 from pypdf import PdfReader
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-fileNamePath = r"C:\Users\mercy\Downloads\Cv George Enike 1.pdf"
+async def extract_pdf_text(file_path: str):
+    logger.info('Extracting text from PDF with filename: %s', file_path)
 
-logger.info('Extracting text from PDF with filename: %s', fileNamePath)
+    reader = await asyncio.to_thread(PdfReader, file_path)
+    page_count = len(reader.pages)
+    all_text = ""
 
-reader = PdfReader(fileNamePath)
-pageNumber = len(reader.pages)
-text = ""
+    for i in range(page_count):
+        page = reader.pages[i]
+        text = await asyncio.to_thread(page.extract_text)
+        all_text += text or ""
 
-for i in range(pageNumber):
-    page = reader.pages[i]
-    text += page.extract_text()
+    logger.info('Successfully extracted text from PDF with filename: %s', file_path)
 
-logger.info('Successfully extracted text from PDF with filename: %s', fileNamePath)
+if __name__ == "__main__":
+    file_path = r"C:\Users\mercy\Downloads\Cv George Enike 1.pdf"
+    asyncio.run(extract_pdf_text(file_path))
